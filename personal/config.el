889fecb362fd99c10a9f37ecbdc16fe2b7ed2543
Mike Auclair <mike@mikeauclair.com>
2014-07-09 09:15:21 -0400

    starting customization

---
+++ personal/config.el
@@ -0,0 +1,5 @@
+(setq projectile-completion-system 'grizzl)
+(setq exec-path (append exec-path '("/usr/local/bin/")))
+(global-set-key (kbd "<home>") 'move-beginning-of-line)
+(global-set-key (kbd "<end>") 'move-end-of-line)
+(setq-default indent-tabs-mode nil)
